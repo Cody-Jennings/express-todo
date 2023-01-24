1591714e9187bc7ab264d78f3221734fc8f268bc
Cody Jennings <cojack91@gmail.com>
2023-01-24 14:47:39 -0500

    rendering practice

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db = mongoose.connection
 const todosData = require('./utilities/data')
 const todosController = require('./controllers/todos')
 
-// Environmental Varibles
+// Environmental Variables
 const app = express()
 const mongoURI = process.env.MONGODB_URI
 const PORT = process.env.PORT || 3001
@@ -18,18 +18,16 @@ mongoose.connect(mongoURI, { useNewUrlParser: true},
 
 // Error / Disconnection
 db.on('error', err => console.log(err.message + ' is Mongod not running?'))
-//db.on("open", () => console.log("mongo connected: ", mongoURI));
 db.on('disconnected', () => console.log('mongo disconnected'))
 
 // Middleware
 app.use(express.urlencoded({ extended: false }))// extended: false - does not allow nested objects in query strings
 app.use(express.json()); //use .json(), not .urlencoded()
 app.use(express.static('public')) // we need to tell express to use the public directory for static files... this way our app will find index.html as the route of the application! We can then attach React to that file!
-app.use(cors())
+app.use(cors({ origin: '*' })) // used to whitelist requests
 
 // Routes
 app.use('/todos', todosController) // telling server.js to get the routes from controllers/todos.js
-
 
 // Seeding the db
 app.get('/seed', async (req, res) => {
